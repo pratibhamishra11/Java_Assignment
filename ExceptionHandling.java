a640e1be6758ca69759eabc7b21104146e3b5cb3
pratibhamishra11 <pratibha.mishra@accolitedigital.com>
2024-03-14 20:03:17 -0700

    throw exception

---
+++ ExceptionHandling.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ExceptionHandling {
+    void eligibility(int a) {
+        if (a < 18) {
+            throw new ArithmeticException("You are not eligible to vote .");
+        } else {
+            System.out.println("You can vote .");
+        }
+    }
+
+    public static void main(String args[]) {
+        ExceptionHandling ob = new ExceptionHandling();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age :");
+        ob.eligibility(sc.nextInt());
+    }
+}
